8ac4348537618ae1b83a0a40fd3495616213391b
Piseth Meng <150414887+MGPISEHT@users.noreply.github.com>
2025-08-02 22:30:36 +0700

    Learning and add notFound page

---
+++ src/app/products/[productID]/reviews/[reviewID]/not-found.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { usePathname } from "next/navigation";
+
+export default function NotFound() {
+    const pathname = usePathname();
+    const productID = pathname.split('/')[2];
+    const reviewID = pathname.split('/')[4];
+  return (
+    <>
+      <h1>Review {reviewID} not found for product {productID} </h1>
+    </>
+  );
+}
--- src/app/products/[productID]/reviews/[reviewID]/page.tsx
+++ src/app/products/[productID]/reviews/[reviewID]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductReviews({
   params,
 }: {
   params: Promise<{ productID: string; reviewID: string }>;
 }) {
   const { productID, reviewID } = await params;
+  if (parseInt(reviewID) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {reviewID} for product {productID}
