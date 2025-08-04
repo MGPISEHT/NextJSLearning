3ff528c5a12b4189450972b49a2f6a7370473f95
Piseth Meng <150414887+MGPISEHT@users.noreply.github.com>
2025-08-04 08:00:07 +0700

    multiple layout

---
+++ src/app/products/[productID]/reviews/[reviewID]/layout.tsx
@@ -0,0 +1,13 @@
+export default function ProductReviewLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+
+      <h1>Product Review Layout</h1>
+    </>
+  );
+}
